8c221d9ff82c6546829ae708287fae8b2fad02d7
Анастасия Петелина < >
2017-01-11 20:52:49 +0300

    Bootstrap

--- view/index.php
+++ view/index.php
@@ -1,17 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="/test/css/style.css">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 	<title>Вход</title>
 </head>
-<body>
-<form action="profile.php" method="POST">
+<body background="/test/img/fon.jpg">
+<!--<form action="profile.php" method="POST">
 <label>Логин</label>
-<input type="text" name="login" required>
+<input type="text"  required>
 <br><br>
 <label>Пароль</label>
-<input type="password" name="password" required>
+<input type="password" required>
 <br><br>
 <input type="submit">
-</form>
+</form>-->
+
+<div class="container-fluid">
+
+  <div class="container" id="my">
+  	<h2 class="text-center">Авторизация</h2>
+		<form class="navbar-form navbar-left center-block text-center" method="POST" action="/test/profile.php" role="search">
+    		<input type="text" class="form-control" placeholder="Login" name="login"><br>
+    		<input type="password" class="form-control" placeholder="Password" name="password"><br>
+   			 <button type="submit" class="btn btn-default">Войти в систему</button>
+  		</form>
+  	<p class="text-center">Если вы у нас впервые, предлагаем вам <a>зарегистрироваться</a> и повторить попытку</p>
+  	<h6 class="text-center">PetuninProduction©</h6>
+  </div>
+ </div>
+
 </body>
 </html>
--- view/profile.php
+++ view/profile.php
@@ -2,33 +2,49 @@
 <html>
 <head>
 	<title>Профиль</title>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="/test/css/style.css">
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-<h1>Hello <?php echo $UserName;?> </h1>
-<a href="/test/index.php?check_out=true">exit</a>
-<br>
-<hr>
-<form method="POST" enctype="multipart/form-data" action="">
-<input name="name_image" type='text' placeholder="Коментарий к изображению "><br><br>
-<input type="file" name="image">
-<input type="submit" value="upload">
-</form>
-<hr>
-<table>
+<div class="container-fuild">
+	<div class="row">
+			<div class="col-md-1"><img src="/test/img/4.jpg" alt="Аватар" class="img-circle avatar"></div>
+			<div class="col-md-4">
+					<h1>Hello <?php echo $UserName;?> </h1>
+					<a href="/test/index.php?check_out=true" class="MyExit" color="black"><span class="glyphicon glyphicon-log-out"></span> Exit</a>
+			</div>
+			<div class="col-md-3 col-md-offset-4">
+					<form method="POST" class="navbar-form navbar-left" role="search" enctype="multipart/form-data" action="">
+						<input type="file" name="image"><br>
+						<input name="name_image" type='text' class="form-control" placeholder="Название изображения"><br>
+						<input type="submit" class="btn btn-warning" value="upload">
+					</form>
+			</div>
+	</div>
+	<br><br>
+
+<div class="container-fuild" id="main">
+<div class="container center-block" id="content">
+<table class="tableImage">
 
 <?php 
 
 $i=0;
 	foreach ($Images as $key => $value) {
-		if(($i)%3==0){echo '<tr>';}
+		if(($i)%4==0){echo '<tr>';}
 		echo "<td><div><img class='galleryImage' src='".$value->path."'><br><p class='explanation'>".$value->name_image."</p></div></td>";
-		if(($i+1)%3==0){echo '</tr>';}
+		if(($i+1)%4==0){echo '</tr>';}
 		$i++;
 	}
 
 ?>
 
 </table>
+</div>
+</div>
+</div>
 </body>
 </html>
